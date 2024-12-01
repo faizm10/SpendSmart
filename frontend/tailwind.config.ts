0b9f9c544fbba8cbb6343c16bc38874aefdc618c
faizm10 <faizmustansar10@gmail.com>
2024-12-01 09:38:57 -0500

    feat: fixed tailwind.config.ts error

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -11,7 +11,7 @@ const config = {
   prefix: "",
   theme: {
   	container: {
-  		center: 'true',
+		center: true, // boolean instead of string
   		padding: '2rem',
   		screens: {
   			'2xl': '1400px'
