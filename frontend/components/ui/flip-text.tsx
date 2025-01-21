c8b3f49670b0d9d08c40c40315a06059811d02ae
faizm10 <faizmustansar10@gmail.com>
2025-01-21 00:35:40 -0500

    fixed vercel errors

--- frontend/components/ui/flip-text.tsx
+++ frontend/components/ui/flip-text.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { AnimatePresence, motion, Variants } from "motion/react";
+import { AnimatePresence, motion, Variants } from "framer-motion";
 
 import { cn } from "@/lib/utils";
 
