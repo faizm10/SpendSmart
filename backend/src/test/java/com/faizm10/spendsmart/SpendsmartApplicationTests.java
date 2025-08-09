8c4da6625d8fb7cf94439f1d95969b58e171b2c6
faizm10 <faizmustansar10@gmail.com>
2025-08-09 00:51:23 -0400

    setup backend

---
+++ backend/src/test/java/com/faizm10/spendsmart/SpendsmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faizm10.spendsmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendsmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
